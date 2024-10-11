36ba6e9066ac727ccc590075895267536ba77f88
Daniel Gavidia <dgavidia1@gmail.com>
2024-10-11 11:07:51 -0400

    Added functionality in script to alter and delete certain files

--- frontend.sh
+++ frontend.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Create frontend directory and navigate there
-echo "Frontend dir name:"
+echo "Enter frontend directory name:"
 read frontend_dir
 mkdir $frontend_dir
 cd $frontend_dir
@@ -22,3 +22,41 @@ bunx tailwindcss init -p
 
 # Install DaisyUI
 bun add -D daisyui@latest
+
+# Update tailwind config
+cat > tailwind.config.js <<EOL
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+	theme: {
+		extend: {},
+	},
+	plugins: [require("daisyui")],
+	daisyui: {
+		themes: ["light", "dark"],
+	},
+};
+EOL
+
+# Replace index.css with tailwind starter
+cd src
+cat > index.css <<EOL
+@tailwind base; 
+@tailwind components; 
+@tailwind utilities; 
+EOL
+
+# Replace default App.tsx with blank App.tsx
+cat > App.tsx <<EOL
+const App = () => {
+	return <div>App</div>;
+};
+
+export default App;
+EOL
+
+# Delete App.css
+rm App.css
+
+# Go back to parent directory
+cd ../../
